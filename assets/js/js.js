e3d722055679f5735491306324bbead0c90b7432
Shaurya <Shaurya@emorres.local>
2017-10-14 11:58:52 +0530

    Added a little responsiveness

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,6 +1,6 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "20%";
+    document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginRight = "20%";
 }
 
